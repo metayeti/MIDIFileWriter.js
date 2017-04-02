9cf2efd8793eaadd8acff1923f9f11b461ef96ff
Danijel Durakovic <danijel.durakovic@gmail.com>
2017-04-03 00:34:21 +0200

    Fix chords example

--- examples/06_chords/code.js
+++ examples/06_chords/code.js
@@ -20,33 +20,33 @@ function getMIDI() {
 		time: 0,
 		note: 'g4'
 	}).noteOff({
-		time: 48,
+		time: 192,  // time where chord ends
 		note: 'c4'
 	}).noteOff({
-		time: 48,
+		time: 0,    // close other notes at same time
 		note: 'e4'
 	}).noteOff({
-		time: 48,
+		time: 0,    // ...
 		note: 'g4'
 	});	
 
 	track.noteOn({
-		time: 0,
+		time: 96,   // time where chord starts
 		note: 'd4'
 	}).noteOn({
-		time: 0,
+		time: 0,    // open other notes at the same time
 		note: 'f4'
 	}).noteOn({
-		time: 0,
+		time: 0,    // ...
 		note: 'a4'
 	}).noteOff({
-		time: 48,
+		time: 192,
 		note: 'd4'
 	}).noteOff({
-		time: 48,
+		time: 0,
 		note: 'f4'
 	}).noteOff({
-		time: 48,
+		time: 0,
 		note: 'a4'
 	});
 		
