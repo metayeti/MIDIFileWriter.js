30d2087d0d6ea921968aa37660a5306c96078b20
pulzed <danijel.durakovic@gmail.com>
2023-06-19 14:56:55 +0200

    Upgrade build sys

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-INFILE=src/midifilewriter.js
-OUTFILE=dist/midifilewriter.min.js
-uglifyjs $INFILE --compress --mangle --screw-ie8 -o $OUTFILE.tmp
-cat header.txt > $OUTFILE
-cat $OUTFILE.tmp >> $OUTFILE
-rm $OUTFILE.tmp
