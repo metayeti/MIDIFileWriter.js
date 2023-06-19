763c8d9e6dd338ebcabb43eb0263999f71741d74
pulzed <danijel.durakovic@gmail.com>
2023-06-19 14:57:16 +0200

    Remove unused

--- build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set "outfile=dist\midifilewriter.min.js"
-uglifyjs src\midifilewriter.js --compress --mangle --screw-ie8 -o %outfile%.tmp | more
-type header.txt > %outfile%
-type %outfile%.tmp >> %outfile%
-del %outfile%.tmp
